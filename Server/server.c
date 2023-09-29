674056591a9a19e2c3ffb667377c789a67e2fced
Miguel Jaramillo <migueljaramillo879@gmail.com>
2023-09-29 01:58:37 -0500

    Implemente las sesiones

--- Server/server.c
+++ Server/server.c
@@ -4,22 +4,28 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define MAX_CLIENTS 10
+#define MAX_CLIENTS 4
 #define MAX_NICKNAME_LENGTH 20
+#define MAX_SESSIONS 2
 
-// Define a structure to store client information
 struct ClientInfo {
     char nickname[MAX_NICKNAME_LENGTH];
-    struct sockaddr_in client_addr;
+    struct sockaddr_in clientAddr;
 };
 
-void printClients(struct ClientInfo *clients, int num_clients) {
-    printf("Connected Clients:\n");
-    for (int i = 0; i < num_clients; i++) {
+struct Session {
+    struct ClientInfo players[2];
+    int sessionNumber;
+    int isOccupied;
+};
+
+void PrintSessionClients(struct Session *session) {
+    printf("Session %d:\n", session->sessionNumber);
+    for (int i = 0; i < session->isOccupied; i++) {
         printf("Client %d: %s - Socket Structure: %s:%d\n",
-               i + 1, clients[i].nickname,
-               inet_ntoa(clients[i].client_addr.sin_addr),
-               ntohs(clients[i].client_addr.sin_port));
+               i + 1, session->players[i].nickname,
+               inet_ntoa(session->players[i].clientAddr.sin_addr),
+               ntohs(session->players[i].clientAddr.sin_port));
     }
 }
 
@@ -29,89 +35,108 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    int server_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+    int serverSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t clientLen = sizeof(clientAddr);
 
-    // Create a UDP socket
-    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_socket < 0) {
+    serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (serverSocket < 0) {
         perror("Socket creation error");
         exit(1);
     }
 
-    // Bind the server socket
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(argv[1]));
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(atoi(argv[1]));
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
         perror("Binding error");
-        close(server_socket);
+        close(serverSocket);
         exit(1);
     }
 
     printf("Server is running and listening on port %d...\n", atoi(argv[1]));
 
     struct ClientInfo clients[MAX_CLIENTS];
-    int num_clients = 0;
+    int numClients = 0;
+
+    struct Session sessions[MAX_SESSIONS];
+    int numSessions = 0;
 
     while (1) {
         char buffer[1024];
-        int bytes_received = recvfrom(server_socket, buffer, sizeof(buffer), 0,
-                                      (struct sockaddr *)&client_addr, &client_len);
-        if (bytes_received < 0) {
+        int bytesRecieved = recvfrom(serverSocket, buffer, sizeof(buffer), 0,
+                                      (struct sockaddr *)&clientAddr, &clientLen);
+        if (bytesRecieved < 0) {
             perror("Error receiving data");
             continue;
         }
 
-        buffer[bytes_received] = '\0';
+        buffer[bytesRecieved] = '\0';
 
-        // Extract the client's nickname from the received message
         char nickname[MAX_NICKNAME_LENGTH];
         sscanf(buffer, "%s", nickname);
 
-        // Find the client in the array by nickname
-        int client_index = -1;
-        for (int i = 0; i < num_clients; i++) {
+        int nicknameExists = 0;
+        for (int i = 0; i < numClients; i++) {
             if (strcmp(clients[i].nickname, nickname) == 0) {
-                client_index = i;
+                nicknameExists = 1;
                 break;
             }
         }
 
-        if (client_index != -1) {
-            // If the client is found, update the sockaddr_in
-            memcpy(&clients[client_index].client_addr, &client_addr, sizeof(client_addr));
-
-            // Handle the rest of the client's messages and data here
+        if (nicknameExists) {
+            printf("Nickname %s already exists. Ignoring new connection request.\n", nickname);
         } else {
-            // If the client is not found, it's a new client, so create a new entry
-            if (num_clients < MAX_CLIENTS) {
-                strncpy(clients[num_clients].nickname, nickname, MAX_NICKNAME_LENGTH);
-                memcpy(&clients[num_clients].client_addr, &client_addr, sizeof(client_addr));
-                num_clients++;
+            if (numClients < MAX_CLIENTS) {
+                strncpy(clients[numClients].nickname, nickname, MAX_NICKNAME_LENGTH);
+                memcpy(&clients[numClients].clientAddr, &clientAddr, sizeof(clientAddr));
+                numClients++;
 
-                // Print all connected clients
-                printClients(clients, num_clients);
+                int sessionIndex = -1;
+                for (int i = 0; i < numSessions; i++) {
+                    if (sessions[i].isOccupied < 2) {
+                        sessionIndex = i;
+                        break;
+                    }
+                }
 
-                // Send a welcome message back to the client
-                char welcome_msg[1024];
-                snprintf(welcome_msg, sizeof(welcome_msg),
-                         "%s welcome! This is your socket structure: %s:%d\n",
-                         nickname, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                if (sessionIndex == -1 && numSessions < MAX_SESSIONS) {
+                    sessionIndex = numSessions;
+                    sessions[sessionIndex].sessionNumber = numSessions;
+                    numSessions++;
+                }
 
-                sendto(server_socket, welcome_msg, strlen(welcome_msg), 0,
-                       (struct sockaddr *)&client_addr, client_len);
+                if (sessionIndex != -1) {
+                    struct Session *currentSession = &sessions[sessionIndex];
+                    if (currentSession->isOccupied < 2) {
+                        currentSession->players[currentSession->isOccupied] = clients[numClients - 1];
+                        currentSession->isOccupied++;
+
+                        char welcomeMsg[1024];
+                        snprintf(welcomeMsg, sizeof(welcomeMsg),
+                                 "%s welcome to Session %d! This is your socket structure: %s:%d\n",
+                                 nickname, currentSession->sessionNumber,
+                                 inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+
+                        sendto(serverSocket, welcomeMsg, strlen(welcomeMsg), 0,
+                               (struct sockaddr *)&clientAddr, clientLen);
+                    }
+                }
             } else {
                 printf("Maximum number of clients reached. Ignoring new connection request.\n");
             }
         }
+
+        for (int i = 0; i < numSessions; i++) {
+            if (i < numSessions) {
+                PrintSessionClients(&sessions[i]);
+            }
+        }
     }
 
-    // Close the server socket (this part will never be reached in this example)
-    close(server_socket);
+    close(serverSocket);
 
     return 0;
 }
