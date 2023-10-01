4e6b4ca4ca8370e2ab4692da335a8abd2769bb82
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-01 16:09:38 -0500

    Cambie el maximo score a 5

--- Client/resources/score.h
+++ Client/resources/score.h
@@ -3,12 +3,12 @@ int CheckScore(int score1, int score2)
 
     int i;
 
-    if (score1 == 10)
+    if (score1 == 5)
     {
 
         return 1;
     }
-    else if (score2 == 10)
+    else if (score2 == 5)
     {
 
         return 2;
