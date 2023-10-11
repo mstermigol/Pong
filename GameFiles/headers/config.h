ae866d39dc44bb2884ad13229038b7cfa1668d8b
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-11 12:18:15 -0500

    Refactored config components

---
+++ GameFiles/headers/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MAX_SESSIONS 2
+#define MAX_NICKNAME_LEN 20
+#define WINNING_SCORE 5
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define PADDLE_WIDTH 10
+#define PADDLE_HEIGHT 50
+#define PADDLE_1_X 10
+#define PADDLE_2_X 620
+#define BALL_WIDTH 10
+#define BALL_HEIGHT 10
+#define LOG_FILE "log.txt"
+
+#endif // CONFIG_H
