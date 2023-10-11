9923dfa6690a36b58872d18e4e1c7c7eaab4034a
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-11 12:16:20 -0500

    Moved Protocol to root

---
+++ Protocol/scc.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *Send(GameState game) {}
+
+char *Send(int upOrDown, int player) {}
+
+char *Send(int state) {}
+
+int Receive(char *buffer) {}
